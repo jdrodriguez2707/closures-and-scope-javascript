6ed1c080e3adb48d10a85e8df8157b539a247e67
jdrodriguez2707 <johandavid.jdr@gmail.com>
2024-04-30 18:10:46 -0500

    Hoisting

---
+++ src/hoisting/index.js
@@ -0,0 +1,20 @@
+// var dogName = undefined; -> implicit
+// console.log(dogName);
+// var dogName = "Frosty";
+
+// showDogName();
+
+// function showDogName() {
+//   console.log(`The best dog is ${dogName}`);
+// }
+
+// var dogName = "Frosty";
+
+if (true) {
+  var greeting = "hi"; // global scope
+  let farewell = "bye";
+}
+
+console.log(greeting);
+console.log(farewell);
+
